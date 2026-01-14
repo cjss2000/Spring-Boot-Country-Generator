5c0e1870b204a8f32e5283de7a7c236827245a54
cjss2000 <cjss2000@gmail.com>
2026-01-14 17:21:45 +0100

    Initial commit

---
+++ src/main/java/org/example/springbootcountrygenerator/SpringBootCountryGeneratorApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootcountrygenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCountryGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCountryGeneratorApplication.class, args);
+    }
+
+}
