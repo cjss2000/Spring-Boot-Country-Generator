63edd4176301e9c953e19382642b47710d0d9cfc
cjss2000 <cjss2000@gmail.com>
2026-07-02 12:41:43 +0200

    added random number method, also resolved bound issue with random.nextInt method calling 0.

--- src/main/java/org/example/springbootcountrygenerator/starwars/FilmService.java
+++ src/main/java/org/example/springbootcountrygenerator/starwars/FilmService.java
@@ -4,11 +4,13 @@ import org.example.springbootcountrygenerator.starwars.response.ApiFilmResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Random;
+
 @Service
 
 public class FilmService {
 
-    private static final String SW_FILM_URL = "https://www.swapi.tech/api/films/1";
+    private static final String SW_FILM_URL = "https://www.swapi.tech/api/films/";
     private final RestTemplate restTemplate;
 
     public FilmService(RestTemplate restTemplate) {
@@ -19,10 +21,15 @@ public class FilmService {
 
 
     public FilmDTO getFilmDTO(){
-            ApiFilmResponse response = restTemplate.getForObject(SW_FILM_URL, ApiFilmResponse.class);
+            ApiFilmResponse response = restTemplate.getForObject(SW_FILM_URL + randomNumber(), ApiFilmResponse.class);
             return response.getResult().getProperties();
         }
 
+        public int randomNumber(){
+            Random random = new Random();
+            int randomInt = random.nextInt(6) + 1;
+            return  randomInt;
+        }
 
     }
 
